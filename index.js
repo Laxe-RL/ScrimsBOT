3743b6e61bc64d1e43ddf2199dcbc455c16a1994
Laxe-RL <85634176+Laxe-RL@users.noreply.github.com>
2021-12-20 16:30:54 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var Discord = require("discord.js");
+var Client = new Discord.Client({intents: []});
+
+Client.on("ready", () => {
+    console.log("bot opérationel");
+});
+
+Client.login("OTIyNDk4MjM5MDU0MzQ4Mjk4.YcCVbw.y8Z77CZE-K-fnjlyKcTKBaT7hBY");
+
+
+
+
+
+
+
+
