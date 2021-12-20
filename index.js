0eac208f8f014775ddfdfdceda60afe58449bf16
Laxe-RL <85634176+Laxe-RL@users.noreply.github.com>
2021-12-20 16:46:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ Client.on("ready", () => {
     console.log("bot opérationel");
 });
 
-Client.login("OTIyNDk4MjM5MDU0MzQ4Mjk4.YcCVbw.y8Z77CZE-K-fnjlyKcTKBaT7hBY");
+Client.login(process.env.TOKEN);
 
 
 
